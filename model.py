f590f1851d337a5a11966a27c07c7dec47defef9
eunsung <eunsung.shin@gmail.com>
2023-09-01 22:34:32 +0900

    follow mlsdp book instruction

--- model.py
+++ model.py
@@ -1,21 +1,21 @@
 from database import Base
-from sqlalchemy import Column, String, Integer, ForeignKey, DateTime
+from sqlalchemy import Column, String, Integer, ForeignKey, Text, DateTime
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.functions import current_timestamp
 from datetime import datetime
 
 
 class Project(Base) :
-    __tablename__ = 'project'
+    __tablename__ = 'projects'
 
-    id = Column('id', Integer, primary_key = True, autoincrement = True)
-    projectname = Column('projectname', String)
-    description = Column('description', String, nullable = True)
-    created_at = Column('created_at', DateTime, server_default = current_timestamp())
+    project_id = Column(String, primary_key = True, comment = 'pk')
+    project_name = Column(String, nullable = False, unique = True, comment = '프로젝트명')
+    description = Column(Text, nullable = True, comment = '설명')
+    created_datetime = Column('created_at', DateTime, server_default = current_timestamp())
 
 
 class Model(Base) :
-    __tablename__ = 'model'
+    __tablename__ = 'models'
 
     id = Column(Integer, primary_key = True, autoincrement = True)
     modelname = Column('modelname', String)
@@ -25,7 +25,7 @@ class Model(Base) :
 
 
 class Experiment(Base) :
-    __tablename__ = 'experiment'
+    __tablename__ = 'experiments'
 
     id = Column(Integer, primary_key = True, autoincrement = True)
     modelid = Column(Integer, ForeignKey('model.id'))
